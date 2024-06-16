b6f879da127273b0afacbe9196af3c4c300f26e9
DishaVaish <123331679+DishaVaish@users.noreply.github.com>
2024-06-16 12:47:16 +0530

    Update applicationRoutes.js

--- Backend/routes/applicationRoutes.js
+++ Backend/routes/applicationRoutes.js
@@ -1,14 +1,1 @@
-import express from "express";
-import {
-  jobSeekerDeleteApplication,
-  jobSeekerGetAllApplications,
-  employerGetAllApplications,
-  postApplication,
-} from "../controllers/applicationController.js";
-import { isAuthorized } from "../middleware/auth.js";
-const router = express.Router();
-router.post("/post", isAuthorized, postApplication);
-router.get("/jobseeker/getall", isAuthorized, jobSeekerGetAllApplications);
-router.get("/employer/getall", isAuthorized, employerGetAllApplications);
-router.delete("/delete/:id", isAuthorized, jobSeekerDeleteApplication);
-export default router;
+
